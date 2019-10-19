059cbe1c0c4b33d27170292c83f597150b840e42
GetanehYilma <gyletike@mum.edu>
2019-10-19 18:09:29 -0500

    initial structure

---
+++ src/main/java/edu/mum/mercato/MercatoApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.mercato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercatoApplication.class, args);
+    }
+
+}
