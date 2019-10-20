aa359eee902ad9deddde866d4c7ac710fecbe97b
GetanehYilma <gyletike@mum.edu>
2019-10-19 23:13:08 -0500

    Domain, Repository, Service, ServiceImpl, HomePage -works

--- src/main/java/edu/mum/mercato/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mum.mercato.repository;
-
-import edu.mum.mercato.domain.Cart;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends CrudRepository<Cart, Long> {
-}
