c957a505f3276c8bb23334f959fd23e40918893d
GetanehYilma <gyletike@mum.edu>
2019-10-19 22:37:51 -0500

    Repositorries, Services and ServiceImpl interfaces add classes are created

--- src/main/java/edu/mum/mercato/serviceImpl/CouponServiceImpl.java
+++ src/main/java/edu/mum/mercato/serviceImpl/CouponServiceImpl.java
@@ -1,8 +1,12 @@
 package edu.mum.mercato.serviceImpl;
 
+import edu.mum.mercato.repository.CouponRepository;
 import edu.mum.mercato.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CouponServiceImpl implements CouponService {
+    @Autowired
+    private CouponRepository couponRepository;
 }
